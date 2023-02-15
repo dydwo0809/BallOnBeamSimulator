055d11762675d045d804061502a7fdf3bfefbf60
dydwo0809 <dydwo0809@naver.com>
2023-02-15 11:50:17 +0900

    동적 그래프 로직 완성

--- Assets/script/DynamicGraph.cs
+++ Assets/script/DynamicGraph.cs
@@ -8,13 +8,18 @@ public class DynamicGraph : MonoBehaviour
     public GameObject DynamicI;
     public GameObject DynamicD;
 
-    public float LengthP;
-    public float LengthI;
-    public float LengthD;
+    private float LengthP;
+    private float LengthI;
+    private float LengthD;
 
-    private float LengthAdjustment = 200;
+    private float LengthAdjustment = 190f;
 
     public GameObject PIDButton;
+
+    public GameObject ball;
+
+    private float DynamicGraphDegree;
+    public GameObject beam;
 
     servo servo;
 
@@ -35,9 +40,44 @@ public class DynamicGraph : MonoBehaviour
         DynamicI.transform.localScale = new Vector3(Mathf.Abs(LengthI), 0.25f, 10.0f);
         DynamicD.transform.localScale = new Vector3(Mathf.Abs(LengthD), 0.25f, 10.0f);
 
-        DynamicP.transform.localPosition = new Vector3(LengthP / 2, 0.25f, 0f);
-        DynamicI.transform.localPosition = new Vector3(LengthI / 2, 0f, 0f);
-        DynamicD.transform.localPosition = new Vector3(LengthD / 2, -0.25f, 0f);
+        if (LengthP > 0)
+        {
+            DynamicP.transform.localPosition = new Vector3(LengthP / 2 + 0.5f, 0.25f, 0f);
+        }
+        else if (LengthP < 0)
+        {
+            DynamicP.transform.localPosition = new Vector3(LengthP / 2 - 0.5f, 0.25f, 0f);
+        }
+        else
+        {
+            DynamicP.transform.localPosition = new Vector3(LengthP / 2, 0.25f, 0f);
+        }
+        if (LengthI > 0)
+        {
+            DynamicI.transform.localPosition = new Vector3(LengthI / 2 + 0.5f, 0f, 0f);
+        }
+        else if(LengthI < 0)
+        {
+            DynamicI.transform.localPosition = new Vector3(LengthI / 2 - 0.5f, 0f, 0f);
+        }
+        else
+        {
+            DynamicI.transform.localPosition = new Vector3(LengthI / 2, 0f, 0f);
+        }
+        if (LengthD > 0)
+        {
+            DynamicD.transform.localPosition = new Vector3(LengthD / 2 + 0.5f, -0.25f, 0f);
+        }
+        else if(LengthD < 0)
+        {
+            DynamicD.transform.localPosition = new Vector3(LengthD / 2 - 0.5f, -0.25f, 0f);
+        }
+        else
+        {
+            DynamicD.transform.localPosition = new Vector3(LengthD / 2, -0.25f, 0f);
+        }
 
+        transform.position = ball.transform.position;
+        transform.eulerAngles = beam.transform.eulerAngles;
     }
 }
